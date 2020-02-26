8e8bc25dcae21ab7a53e8da03f92e279fa2cc243
Ayrton DE ABREU MIRANDA <asadams89@gmail.com>
2020-02-26 14:32:23 +0100

    Importation de config-server

---
+++ config-server/config-server/src/test/java/com/hyl/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyl.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
