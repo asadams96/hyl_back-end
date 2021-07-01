538abe0f6e674b4746d54661bd096557958fc7b3
Ayrton DE ABREU MIRANDA <asadams89@gmail.com>
2021-07-01 18:23:28 +0200

    batch : Ajout de la dépendance OpenFeign dans le pom.xml + Création de l'interface 'MailProxy' + Ajout de la configuration dans la classe main 'BatchApplication'

--- dev/batch/src/main/java/com/hyl/batch/BatchApplication.java
+++ dev/batch/src/main/java/com/hyl/batch/BatchApplication.java
@@ -2,7 +2,9 @@ package com.hyl.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(value = "com.hyl.batch.proxy")
 @SpringBootApplication
 public class BatchApplication {
 
---
+++ dev/batch/src/main/java/com/hyl/batch/proxy/MailProxy.java
@@ -0,0 +1,18 @@
+package com.hyl.batch.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.HashMap;
+
+@Component
+@FeignClient(name = "mail-server", url = "${hyl.gateway.url}"+"/mail")
+public interface MailProxy {
+
+    @PostMapping(value = "/send-mail")
+    void sendMail(@RequestHeader HashMap<String, String> header,
+                  @RequestBody HashMap<String, String> body);
+}
