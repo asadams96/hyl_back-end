5e884a2bb30b530d8622c5e4c1b9194a98881b79
Ayrton DE ABREU MIRANDA <asadams89@gmail.com>
2021-07-07 14:41:27 +0200

    memo-api : Ajout d'un logger dans le service 'MemoService'

--- dev/memo-api/src/main/java/com/hyl/memoapi/service/MemoService.java
+++ dev/memo-api/src/main/java/com/hyl/memoapi/service/MemoService.java
@@ -1,9 +1,15 @@
 package com.hyl.memoapi.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemoService {
+
+    //************************************************ LOGGER
+    Logger logger = LoggerFactory.getLogger(MemoService.class);
+
 }
